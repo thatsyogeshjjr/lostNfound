87204850c8e668deccd418a63dac51f6b8615e65
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-06-03 19:50:03 +0530

    added navbar and made vite config to host at 0.0.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <h1 className="font-bold text-xl">Hello World!</h1>
     </>
   );
