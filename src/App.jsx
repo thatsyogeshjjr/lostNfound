b105f2bee3497e21f4f6dd5325df4391e55d1a69
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-06-08 11:30:28 +0530

    added banner and buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import "./App.css";
-import { Banner } from "./components/Banner";
-import { Report } from "./components/Report";
+import { Navbar } from "./components/Navbar";
+import BannerImg from "./assets/cover.jpg";
 
 function App() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center w-screen m-2">
-        <Banner />
-        <Report />
+      <div>
+        <Navbar />
+        <div
+          id="hero"
+          className="w-screen flex flex-col justify-center items-center my-8 gap-12"
+        >
+          <div
+            id="banner"
+            alt="Banner Image"
+            className="h-80 w-[80vw] rounded-lg flex justify-start p-10 items-center"
+          ></div>
+          <div
+            id="cta"
+            className="flex flex-row gap-5 *:border-2 *:text-md *:px-10 *:py-2 *:rounded-full *:text-black *:bg-white *:border-solid *:border-[#9767c2]"
+          >
+            <button className="">Report a lost Item</button>
+            <button className="">I found something</button>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 export var Navbar = () => {
   return (
     <>
-      <nav className="flex flex-row justify-between px-6 py-4 bg-[#978897] text-slate-50 top-0 p-2 sticky">
-        <h1 className="text-xl">Lost and Found Dept.</h1>
-        <ul className="flex flex-row text-lg gap-4 *:border-[solid] *:border-2 *:border-white *:rounded-full *:px-4 *:py-[0.5px]">
+      <nav className="flex flex-row justify-between w-screen px-10 text-top-0 text-[#978897] py-4 p-2 sticky">
+        <h1 className="text-2xl">Lost and Found Dept.</h1>
+        <ul className="flex flex-row text-lg gap-4 *:border-[solid] *:border-2 *:border-slate-500 *:rounded-full *:px-4 *:py-[0.5px]">
           <li>Unclaimed Bids</li>
           <li>FAQ</li>
           <li>Contact Us</li>
